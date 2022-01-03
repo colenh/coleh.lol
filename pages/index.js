c6961e14b7f75e7c5d52acd9633fb3016223affe
cole <40342475+colenh@users.noreply.github.com>
2022-01-03 01:35:29 -0500

    remove treehous

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,7 @@ function Home () {
     <div>
       <h1 className='text-4xl font-calibri font-bold mb-6'>Who Am I?</h1>
       <p className='text-2xl font-calibri font-normal mb-6'>I am a 14y.o. full-stack indie developer who loves NodeJS and NextJS. <br />
-      I design, build, and ship products of quality and reliability. <br />
-      Currently, I'm a founder and developer at <Link href='https://treehous.app' passHref><span className='text-thGreen cursor-pointer treehous'>Treehous</span></Link>.</p>
-      <h3 className='text-2xl font-calibri font-semibold'>What is Treehous?</h3>
-      <p className='text-xl font-calibri font-normal mb-6'>Treehous is a new social app where you can post images, text, chat with friends, find new friends, and have fun. <br />
-      Treehous is in a closed alpha phase, you can check it out at <Link href='https://treehous.app' passHref><span className='text-thGreen cursor-pointer treehous'>treehous.app</span></Link>.</p>
+      I design, build, and ship products of quality and reliability. </p>
     </div>
   );
 };
